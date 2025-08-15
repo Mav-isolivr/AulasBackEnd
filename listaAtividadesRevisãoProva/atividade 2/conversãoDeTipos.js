022488aba05f53e51db9617048cabe3b30787190
Aluno SENAI <Aluno@SN-351174.edu.fiesc.com.br>
2025-08-14 21:31:20 -0300

    atividades para revisão da prova

---
+++ listaAtividadesRevisãoProva/atividade 2/conversãoDeTipos.js
@@ -0,0 +1,5 @@
+let numero = 42
+let conv = Number(numero)
+let soma = (conv + 10)
+
+console.log(soma)
---
+++ listaAtividadesRevisãoProva/atividade1/variaveisEtiposDeDados.js
@@ -0,0 +1,12 @@
+const nome = "Maria Vithória";
+const idade = 17;
+const gostaDeProgramar = "Depende";
+const altura = "1.60"
+
+const corFavorita = ["roxo","amarelo", "azul"];
+
+const objt = {
+    nome,idade
+}
+
+console.log(objt)
